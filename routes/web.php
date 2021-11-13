f234d292ef009f15b604c7fc195e88330e9e9a6a
rivaldonamare <rivaldonamare8@gmail.com>
2021-11-13 12:13:36 +0700

    bikin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontendController;
 
 use Illuminate\Support\Facades\Route;
@@ -16,7 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FrontendController::class, 'index'])->name('index');
 Route::get('/details/{slug}', [FrontendController::class, 'details'])->name('details');
+Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
+Route::get('/checkout/success', [FrontendController::class, 'success'])->name('checkout-success');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->name('dashboard.')->prefix('dashboard')->group(function (){
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
+    Route::middleware(['admin'])->group(function(){
+
+    });
+});
