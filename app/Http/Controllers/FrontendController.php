9cbfbc322b11e6e6cdbcb1963f4d8b2790c959b5
nadiadea <nadia.hassanah@gmail.com>
2021-12-02 23:32:38 +0700

    build recommendation

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,20 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+
 
 class FrontendController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.frontend.index');
+        $products = Product::with(['galleries'])->latest()->get();
+        return view('pages.frontend.index', compact('products'));
     }
     
     public function details(Request $request, $slug)
     {
-        return view('pages.frontend.details');
+        $products = Product::with(['galleries'])->where('slug',$slug)->firstOrFail();
+        $recommendations = Product::with(['galleries'])->inRandomOrder()->limit(4)->get();
+        return view('pages.frontend.details',compact('products','recommendations'));
     }
 
+    public function cartAdd(Request $request, $id)
+    {
+        Cart::create([
+            'users_id' => Auth::user()->id,
+            'products_id' => $id
+        ]);
+        return redirect('cart');
+    }
+        
     public function cart(Request $request)
     {
         return view('pages.frontend.cart');
--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FrontendController::class, 'index'])->name('index');
 Route::get('/details/{slug}', [FrontendController::class, 'details'])->name('details');
-Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
-Route::get('/checkout/success', [FrontendController::class, 'success'])->name('checkout-success');
 
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function (){
+    Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
+    Route::post('/cart/{id}', [FrontendController::class, 'cartAdd'])->name('cart-add');
+    Route::get('/checkout/success', [FrontendController::class, 'success'])->name('checkout-success');
+});
 
 Route::middleware(['auth:sanctum', 'verified'])->name('dashboard.')->prefix('dashboard')->group(function (){
     Route::get('/', [DashboardController::class, 'index'])->name('index');
